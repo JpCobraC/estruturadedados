5be5ec6087df76504aaca1c40093b0f3fb458e59
JpCobraC <jpcobrac@gmail.com>
2026-03-06 17:57:12 -0300

    Ativ01

--- Atividade01.py
+++ Atividade01.py
@@ -20,6 +20,8 @@ class DynamicIntArray:
             self._resize(self.capacity * 2)
 
         self.data[self.size] = value
+        self.size += 1
+
 
     def _resize(self, new_capacity):
         if new_capacity > self.capacity:
